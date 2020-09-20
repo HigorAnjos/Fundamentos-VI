dfc146ead47f2ecef7997c84905da4a6de845b9f
HigorAnjos <higorc.anjos@gmail.com>
2020-09-20 17:50:23 -0300

    up

---
+++ reta_tangente_ao_ponto_da_funcao.py
@@ -0,0 +1,32 @@
+import numpy as np
+from sympy import *
+from math import *
+import matplotlib.pyplot as pl
+x1 = 3
+
+x = Symbol('x')
+
+f = x**2
+fx = lambdify(x, f, modules=['numpy'])
+
+d = diff(fx(x), x)
+df = lambdify(x, d,  modules=['numpy'])
+
+print(f'Funcao {fx(x)}')
+print(f'Derivada {df(x)} (coeficiente agular m)')
+
+y1 = fx(x1)
+#  (x1, y1)
+#  Y - Y1 = m (X - X1)
+
+funcao = df(x1)*x - df(x1)*x1 + y1
+
+reta_tangente = lambdify(x, funcao, modules=['numpy'])
+
+
+grafico_val = np.linspace(-100,100,2000)
+grafico_reta = np.linspace(-10,10,100)
+pl.plot(grafico_val, fx(grafico_val)) #  grafico da funcao
+pl.plot(x1, fx(x1), 'ro')  #  Ponto x1 da funcao
+pl.plot(grafico_val, reta_tangente(grafico_val))
+pl.show()
