a27625f8150ea1569cf5d4501ceba4252e08939b
HigorAnjos <higorc.anjos@gmail.com>
2020-09-30 13:20:48 -0300

    Update Nao linear.py

--- Nao linear.py
+++ Nao linear.py
@@ -5,11 +5,11 @@ from numpy.linalg import solve
 x1 = Symbol('x1')
 x2 = Symbol('x2')
 # Ponto de inicio do x0
-x = np.array((1.5, 1.5))
+x = np.array((1, 5))
 
 # Partir da matriz de jacob
 fx1 = x1 + x2 - 3
-fx2 = x1 ** 2 - x2 ** 2 - 9
+fx2 = x1**2 + x2**2 - 9
 
 f1 = lambdify(x1, fx1, modules=['numpy'])
 f2 = lambdify(x1, fx2, modules=['numpy'])
@@ -31,7 +31,7 @@ f1_aux = lambdify(x2, fx1, modules=['numpy'])
 D_x2 = diff(f1_aux(x2).subs(x1, 1), x2)
 f1_d_x2 = lambdify(x2, D_x2, modules=['numpy'])
 
-#print(f"{D_x1} {D_x2}")
+print(f"{D_x1} {D_x2}")
 
 #   f2
 # f2 derivada em x1
@@ -44,19 +44,19 @@ f2_aux = lambdify(x2, fx2, modules=['numpy'])
 D_x2 = diff(f2_aux(x2).subs(x1, 1), x2)
 f2_d_x2 = lambdify(x2, D_x2, modules=['numpy'])  # chama f1_d_x1 para passar o valor de x1 em funcao f1 devirada em x1
 
-#print(f"{D_x1} {D_x2}")
+print(f"{D_x1} {D_x2}")
 
 jacobiana = np.array(([float(f1_d_x1(x[0])), float(f1_d_x2(x[1]))],
                       [float(f2_d_x1(x[0])), float(f2_d_x2(x[1]))]))
 
 vetor_f_x = np.array((float(f1(x[0]).subs(x2, x[1])), float(f2(x[0]).subs(x2, x[1]))))
-#print(vetor_f_x) F(x0)
+print(vetor_f_x) # F(x0)
 
 s0 = np.linalg.solve(jacobiana, -vetor_f_x)
 
 x = x + s0
-#print(f"Primeira interacao {x}")
-
+print(f"Primeira interacao {x}")
+intecacoes = 1
 vetor_f_x = np.array((float(f1(x[0]).subs(x2, x[1])), float(f2(x[0]).subs(x2, x[1]))))
 while np.linalg.norm(vetor_f_x) > 0.0001:
     jacobiana = np.array(([float(f1_d_x1(x[0])), float(f1_d_x2(x[1]))],
@@ -65,5 +65,6 @@ while np.linalg.norm(vetor_f_x) > 0.0001:
     s0 = np.linalg.solve(jacobiana, -vetor_f_x)
     x = x + s0
     vetor_f_x = np.array((float(f1(x[0]).subs(x2, x[1])), float(f2(x[0]).subs(x2, x[1]))))
+    intecacoes += 1
 
-print(f"Resposta {x}")
+print(f"Resposta {x} interacoes {intecacoes}")
